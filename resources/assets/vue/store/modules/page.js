17d856806473b928afe8ead10fa1cc66a051c2f3
Yuriy Arkhipov <omadonex@yandex.ru>
2019-02-05 18:28:18 +0300

    wip

--- resources/assets/vue/store/modules/page.js
+++ resources/assets/vue/store/modules/page.js
@@ -1,8 +1,18 @@
 const state = {
+  data: {},
   index: 0,
 };
+
+const mutations = {
+  updateData(state, data) {
+    for (let prop in data) {
+      state.data = { ...state.data, [prop]: data[prop] };
+    }
+  }
+}
 
 export default {
   namespaced: true,
   state,
+  mutations,
 };
