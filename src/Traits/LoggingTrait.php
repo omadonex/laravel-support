da4a23d7e5804a74d8b7c065de49a0d702ab2e46
Yuriy Arkhipov <omadonex@yandex.ru>
2019-03-29 12:31:38 +0300

    wip

---
+++ src/Traits/LoggingTrait.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Omadonex\LaravelSupport\Traits;
+
+use Illuminate\Support\Facades\Log;
+
+trait LoggingTrait
+{
+    private $logging = false;
+
+    public function enableLogging()
+    {
+        $this->logging = true;
+    }
+
+    public function disableLogging()
+    {
+        $this->logging = false;
+    }
+
+    private function log($message, $level = 'info')
+    {
+        $className = (new \ReflectionClass($this))->getShortName();
+        if ($this->logging) {
+            Log::$level("{$className} :: {$message}");
+        }
+    }
+}
