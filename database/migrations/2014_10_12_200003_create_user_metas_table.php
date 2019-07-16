35829c96dd024fd1dc6cb82a059593027007b3d0
Yuriy Arkhipov <omadonex@yandex.ru>
2019-07-16 10:05:06 +0300

    wip

--- database/migrations/2014_10_12_200003_create_user_metas_table.php
+++ database/migrations/2014_10_12_200003_create_user_metas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUserMetas extends Migration
+class CreateUserMetasTable extends Migration
 {
     /**
      * Run the migrations.
